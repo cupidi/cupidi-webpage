c12a669327ded584b421e406cc6454a1e204b377
Mario Ampov <ampov.mario@gmail.com>
2017-07-07 14:34:40 +0200

    Change theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Ng2PageScrollModule } from 'ng2-page-scroll';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,7 +15,6 @@ import { AppRoutingModule } from './app.routing';
   ],
   imports: [
     BrowserModule,
-    Ng2PageScrollModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,13 @@ if (environment.production) {
   enableProdMode();
 }
 
+declare var skel: any;
+skel.breakpoints({
+  xlarge: '(max-width: 1680px)',
+  large: '(max-width: 1280px)',
+  medium: '(max-width: 980px)',
+  small: '(max-width: 736px)',
+  xsmall: '(max-width: 480px)'
+});
+
 platformBrowserDynamic().bootstrapModule(AppModule);
